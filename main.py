71bc127743de48b549557c987323b93558ffdd78
mahmoudjawad02025 <mahmoudjawad02025@gmail.com>
2026-09-08 15:18:50 +0300

    feat: chapter four - lesson one done

--- main.py
+++ main.py
@@ -34,40 +34,56 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-
-    print("2 - - - - - - - - - - - \n")
     messages = [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": args.user_prompt}
     ]
-    response = send_request(messages)
-    message = response.choices[0].message
+
+    print("2 - - - - - - - - - - - \n")
+    
+    for _ in range(20):
+        response = send_request(messages)
+        message = response.choices[0].message
+        messages.append(message)
+        
+        if message.tool_calls:
+            for tool_call in message.tool_calls:
+                function_args = json.loads(tool_call.function.arguments)
+                # print(f"Calling function: {tool_call.function.name}({function_args})")
+                result_message = call_function1(tool_call, args.verbose)
+                messages.append(result_message)
+                if(args.verbose): print(f"-> {result_message['content']}")
+                if len(result_message['content']) == 0:
+                    raise Exception("Tool message content is empty")
+        else:
+            if args.verbose:
+                print(f"User prompt: {args.user_prompt}\n")
+                print(f"Prompt tokens: {response.usage.prompt_tokens}\n")
+                print(f"Response tokens: {response.usage.completion_tokens}")
+            print(message.content)
+            return
+        
     if message.tool_calls:
-        for tool_call in message.tool_calls:
-            function_args = json.loads(tool_call.function.arguments)
-            # print(f"Calling function: {tool_call.function.name}({function_args})")
-            result_message = call_function1(tool_call, args.verbose)
-            if(args.verbose): print(f"-> {result_message['content']}")
-            if len(result_message['content']) == 0:
-                raise Exception("Tool message content is empty")
+        print("Error: Max iterations reached")
+        exit(1)
+        
             
 
-    print("3 - - - - - - - - - - - \n")
-    if response.usage is None :
-        raise RuntimeError("response.usage is None")
+    # print("3 - - - - - - - - - - - \n")
+    # if response.usage is None :
+    #     raise RuntimeError("response.usage is None")
 
 
 
-    print("4 - - - - - - - - - - - \n")
-    if not message.tool_calls:
-        if(not args.verbose):
-            print(response.choices[0].message.content)
-        else:
-            print(f"User prompt: {args.user_prompt}\n")
-            print(f"Prompt tokens: {response.usage.prompt_tokens}\n")
-            print(f"Response tokens: {response.usage.completion_tokens}")
+    # print("4 - - - - - - - - - - - \n")
+    # if not message.tool_calls:
+    #     if(not args.verbose):
+    #         print(response.choices[0].message.content)
+    #     else:
+    #         print(f"User prompt: {args.user_prompt}\n")
+    #         print(f"Prompt tokens: {response.usage.prompt_tokens}\n")
+    #         print(f"Response tokens: {response.usage.completion_tokens}")
 
 
 if __name__ == "__main__":
     main()
-    
